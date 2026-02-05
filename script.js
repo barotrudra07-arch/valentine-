91d0a234590a859f7bb1803e94b87d6c8a171b5e
barotrudra07-arch <barotrudra07@gmail.com>
2026-02-05 15:22:43 +0530

    Refactor noBtn event listener and add music play

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.position = "relative";
   noBtn.style.left = Math.random() * 200 + "px";
 });
+document.addEventListener("click", function () {
+  const music = document.getElementById("bgMusic");
+  if (music.paused) {
+    music.play();
+  }
+}, { once: true });
